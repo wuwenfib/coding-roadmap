915654d00056ee42f00e633516aa3034a7dfc168
wuwenfib <ww18551867763@gmail.com>
2026-06-16 16:56:33 +0800

    最小连续数组

---
+++ algorithm/01-array/LC209_min_subarray.cpp
@@ -0,0 +1,70 @@
+/*
+209. 长度最小的子数组
+中等
+相关标签
+premium lock icon
+相关企业
+给定一个含有 n 个正整数的数组和一个正整数 target 。
+
+找出该数组中满足其总和大于等于 target 的长度最小的 子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
+
+ 
+
+示例 1：
+
+输入：target = 7, nums = [2,3,1,2,4,3]
+输出：2
+解释：子数组 [4,3] 是该条件下的长度最小的子数组。
+示例 2：
+
+输入：target = 4, nums = [1,4,4]
+输出：1
+示例 3：
+
+输入：target = 11, nums = [1,1,1,1,1,1,1,1]
+输出：0
+ 
+
+提示：
+
+1 <= target <= 109
+1 <= nums.length <= 105
+1 <= nums[i] <= 104
+ 
+
+进阶：
+
+如果你已经实现 O(n) 时间复杂度的解法, 请尝试设计一个 O(n log(n)) 时间复杂度的解法
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int minSubArrayLen(int target, vector<int>& nums) {
+    int i = 0;
+    int j = 0;
+    int num_size = nums.size()-1;
+    int total = 0;
+    int res = nums.size() + 1 ;
+    while(j <= num_size){
+        total+=nums[j];
+        while(target <= total){
+            res=min(res,j-i+1);
+            total-=nums[i];
+            i++;
+            
+        }
+        
+        j++;
+    }
+    return res==(nums.size() + 1 )?0:res;
+}
+
+int main(){
+    int target = 7;
+    vector<int> nums{2,3,1,2,4,3};
+    cout<<minSubArrayLen(target,nums)<<endl;
+    return 0;
+}
